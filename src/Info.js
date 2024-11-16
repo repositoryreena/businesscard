a1e618981c92fb98894890962ea61b29b7a60b5e
Reena Thomas <rtthomas@ucsc.edu>
2024-11-15 19:10:25 -0800

    css

--- src/Info.js
+++ src/Info.js
@@ -1,5 +1,6 @@
 import React from "react"
 import image from './images/america.JPG'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Info() {
     return (
@@ -9,8 +10,8 @@ export default function Info() {
         <p>Frontend Developer</p>
         <p>repositoryreena.github.io</p>
         <span>
-        <button>Email</button>
-        <button className="linkedin">Linkedin</button>
+        <button><FontAwesomeIcon icon="fa-solid fa-envelope" />Email</button>
+        <button className="linkedin"><FontAwesomeIcon icon="fa-brands fa-linkedin" />Linkedin</button>
         </span>
         </>
     )
